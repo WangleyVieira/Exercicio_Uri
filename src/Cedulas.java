c677b7fbe509184e59ae12364b05fa421e06fbfc
Wangley Vieira <wangleymsv10@hotmail.com>
2020-04-10 20:09:39 -0300

    Resolução do exercício (1018) UriOnline

--- src/Cedulas.java
+++ src/Cedulas.java
@@ -14,24 +14,28 @@ public class Cedulas {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         
-        int valor = leitor.nextInt();
+        int numero = leitor.nextInt();
+        
         int cont100 = 0, cont50 = 0, cont20 = 0, cont10 = 0, cont5 = 0,
                 cont2 = 0, cont1 = 0;
         
-       
-        
-        
-        
-        
-        
+       if(numero > 0 && numero < 1000000){
+           cont100 = (int)(numero / 100);
+           cont50 = (int)((numero % 100) / 50);
+           cont20 = (int)(((numero % 100) % 50) / 20);
+           cont10 = (int)((((numero % 100) % 50) % 20) / 10);
+           cont5 = (int)(((((numero % 100) % 50) % 20) % 10) / 5);
+           cont2 = (int)((((((numero % 100) % 50) % 20) % 10) % 5) / 2);
+           cont1 = (int)((((((numero % 100) % 50) % 20) % 10) % 5) % 2);
+       }
         
         System.out.println(cont100 +" nota(s) de R$ 100,00");
         System.out.println(cont50 +" nota(s) de R$ 50,00");
         System.out.println(cont20 + " nota(s) de R$ 20,00");
-        //System.out.println(cont10 +" nota(s) de R$ 10,00");
-        //System.out.println(cont5 + " nota(s) de R$ 5,00");
-        //System.out.println(cont2 +" nota(s) de R$ 2,00");
-        //System.out.println(cont1 +" nota(s) de R$ 1,00");
+        System.out.println(cont10 +" nota(s) de R$ 10,00");
+        System.out.println(cont5 + " nota(s) de R$ 5,00");
+        System.out.println(cont2 +" nota(s) de R$ 2,00");
+        System.out.println(cont1 +" nota(s) de R$ 1,00");
         
     }
     
